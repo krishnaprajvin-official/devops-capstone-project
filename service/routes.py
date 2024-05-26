3ef8bfd6a175a45d97c3997e15427961a6ca68fe
krishnaprajvin-official <727822tucs061@skct.edu.in>
2024-05-26 05:19:28 -0400

    Added.flake8 file

--- service/routes.py
+++ service/routes.py
@@ -41,8 +41,7 @@ def index():
 @app.route("/accounts", methods=["POST"])
 def create_accounts():
     """
-    Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
+    Creates an Account This endpoint will create an Account based the data in the body that is posted
     """
     app.logger.info("Request to create an Account")
     check_content_type("application/json")
@@ -81,13 +80,15 @@ def list_accounts():
 def get_accounts(account_id):
     """
     Reads an Account
-    This endpoint will read an Account based the account_id that is requested
+    This endpoint will read an Account based the account_id that 
+    is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
 
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(status.HTTP_404_NOT_FOUND,
+              f"Account with id [{account_id}] could not be found.")
 
     return account.serialize(), status.HTTP_200_OK
 
@@ -104,7 +105,8 @@ def update_accounts(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+        abort(status.HTTP_404_NOT_FOUND,
+              f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
     account.update()
     return account.serialize(), status.HTTP_200_OK
@@ -117,7 +119,8 @@ def update_accounts(account_id):
 def delete_accounts(account_id):
     """
     Delete an Account
-    This endpoint will delete an Account based on the account_id that is requested
+    This endpoint will delete an Account based on the account_id that 
+    is requested
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
     account = Account.find(account_id)
